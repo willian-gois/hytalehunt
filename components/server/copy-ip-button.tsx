137cbe6f291d70104779611f1de4673f419547d7
Willian Gois <willian2o12@hotmail.com>
2026-01-16 20:40:03 -0300

    feat: copy ip button improvements

--- components/server/copy-ip-button.tsx
+++ components/server/copy-ip-button.tsx
@@ -1,31 +1,45 @@
 "use client"
 
 import { RiFileCopyLine } from "@remixicon/react"
+import type { VariantProps } from "class-variance-authority"
 import { toast } from "sonner"
 
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+import { Button, type buttonVariants } from "@/components/ui/button"
 
 interface CopyIpButtonProps {
-  ipAddress: string
-  name: string
+	className?: string
+	ipAddress: string
+	name: string
 }
 
-export function CopyIpButton({ ipAddress, name }: CopyIpButtonProps) {
-  return (
-    <Button
-      variant="outline"
-      size="sm"
-      className="h-9 px-3"
-      onClick={(e) => {
-        e.stopPropagation()
-        e.preventDefault()
-        navigator.clipboard.writeText(ipAddress)
-        toast.success("IP copied to clipboard!")
-      }}
-      title={`Copy ${name} IP address`}
-    >
-      <RiFileCopyLine className="h-4 w-4" />
-      <span>{ipAddress}</span>
-    </Button>
-  )
+export function CopyIpButton({
+	className,
+	ipAddress,
+	name,
+	variant = "default",
+	...props
+}: CopyIpButtonProps &
+	VariantProps<typeof buttonVariants> & {
+		asChild?: boolean
+	}) {
+	return (
+		<Button
+			variant={variant}
+			size="sm"
+			className={cn("h-9 px-3 cursor-copy", className)}
+			onClick={(e) => {
+				e.stopPropagation()
+				e.preventDefault()
+				navigator.clipboard.writeText(ipAddress)
+				toast.success("IP copied to clipboard!")
+			}}
+			title={`Copy ${name} IP address`}
+			{...props}
+		>
+			<RiFileCopyLine className="h-4 w-4" />
+			<span>{ipAddress}</span>
+		</Button>
+	)
 }
