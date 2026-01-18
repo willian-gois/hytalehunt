2e5e345687543b858cc1b44e3cef9209e8cd0e9b
Willian Gois <willian2o12@hotmail.com>
2026-01-18 00:45:27 -0300

    feat: make system as default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
           <PostHogPageview />
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="system"
             enableSystem
             disableTransitionOnChange
           >
